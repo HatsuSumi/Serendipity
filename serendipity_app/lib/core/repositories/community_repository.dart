a8bcdae5b4a68f78790dff9b6676673eb27af1ca
Serendipity Developer <dev@serendipity.app>
2026-03-15 21:47:48 +0800

    Revert tagMatchMode to enum in filterPosts

--- serendipity_app/lib/core/repositories/community_repository.dart
+++ serendipity_app/lib/core/repositories/community_repository.dart
@@ -241,7 +241,7 @@ class CommunityRepository {
       placeTypes: placeTypes?.map((t) => t.value).toList(),
       tags: tags,
       statuses: statuses?.map((s) => s.name).toList(),
-      tagMatchMode: tagMatchMode.value,
+      tagMatchMode: tagMatchMode,
       limit: limit,
     );
   }
